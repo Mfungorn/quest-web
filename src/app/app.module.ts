387ca473e3868b2e7933d40c329ca0568bffa613
Yuri Tsai <yura.tsai@gmail.com>
2019-06-30 22:10:15 +0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MockPageComponent } from './pages/mock-page/mock-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MockPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/mock-page/mock-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-page',
+  templateUrl: './mock-page.component.html',
+  styleUrls: ['./mock-page.component.css']
+})
+export class MockPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
